5a3405117460bd3b4a0b44480800ef37c6361a8f
rasenmaeherjoe <rasenmaeherjoe35@gmail.com>
2021-02-09 21:38:38 +0100

    add type to bar graph add badge for selected weapons count add select none option

--- src/App.js
+++ src/App.js
@@ -67,7 +67,10 @@ function App() {
                 .map( g => <div key={g}>
                   <div className="p-1 my-3 d-flex" style={{ borderBottom: '1px solid darkgray'}}>
                     <div style={{fontSize: '1.1rem'}} className="flex-grow-1">{g}</div>
-                    <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active || w.type === g })))}>select all</div>
+                    { weapons.filter( w => w.type === g).some( w => w.active) ?                    
+                      <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active && w.type !== g })))}>select none</div> :
+                      <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active || w.type === g })))}>select all</div>
+                    }
                   </div>
                   <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                     {weapons.filter( w => w.type === g).map( w => <Weapon key={w.name} onCompare={() => toggleCompare(w)} changeTier={changeTier} data={w} />)}
@@ -78,7 +81,10 @@ function App() {
                 .map( r => <div key={r}>
                   <div className="p-1 my-3 d-flex" style={{ borderBottom: '1px solid darkgray'}}>
                     <div style={{fontSize: '1.1rem'}} className="flex-grow-1">{r}</div>
-                    <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active || w.rogues.includes(r) })))}>select all</div>
+                    { weapons.filter( w => w.rogues.includes(r) ).some( w => w.active) ?
+                      <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active && !w.rogues.includes(r) })))}>select none</div> :
+                      <div style={{color: 'navy', fontSize: '.75rem', cursor: 'pointer'}} onClick={() => setWeapons( weapons.map( w => ({...w, active: w.active || w.rogues.includes(r) })))}>select all</div>
+                    }
                   </div>
                   <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                     {weapons.filter( w => w.rogues.includes(r) ).map( w => <Weapon key={w.name} onCompare={() => toggleCompare(w)} changeTier={changeTier} data={w} />)}
@@ -89,8 +95,9 @@ function App() {
               <span className="d-inline-block p-2 m-2 card shadow-sm" style={{ borderRadius: '15%' }} onClick={ () => clearSelection() }>
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M5.662 23l-5.369-5.365c-.195-.195-.293-.45-.293-.707 0-.256.098-.512.293-.707l14.929-14.928c.195-.194.451-.293.707-.293.255 0 .512.099.707.293l7.071 7.073c.196.195.293.451.293.708 0 .256-.097.511-.293.707l-11.216 11.219h5.514v2h-12.343zm3.657-2l-5.486-5.486-1.419 1.414 4.076 4.072h2.829zm.456-11.429l-4.528 4.528 5.658 5.659 4.527-4.53-5.657-5.657z"/></svg>
               </span>
-              <span className="d-inline-block p-2 m-2 card shadow-sm" style={{ borderRadius: '15%' }} onClick={ () => window.scrollTo(0,graphsEl.current.getBoundingClientRect().y + window.scrollY) }>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 19h-4v-8h4v8zm6 0h-4v-18h4v18zm6 0h-4v-12h4v12zm6 0h-4v-4h4v4zm1 2h-24v2h24v-2z"/></svg>
+              <span className="d-inline-block p-2 m-2 card shadow-sm" style={{ borderRadius: '15%', fontSize: '.8rem' }} onClick={ () => window.scrollTo(0,graphsEl.current.getBoundingClientRect().y + window.scrollY) }>
+                <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 19h-4v-8h4v8zm6 0h-4v-18h4v18zm6 0h-4v-12h4v12zm6 0h-4v-4h4v4zm1 2h-24v2h24v-2z"/></svg>
+                <span class="badge bg-secondary">{weapons.filter( w => w.active).length}</span>
               </span>
             </div>
             <div ref={graphsEl}>
--- src/graphs/bargraph.js
+++ src/graphs/bargraph.js
@@ -18,12 +18,13 @@ const BarGraph = ({ weapons, title, toValue, invert, ...props }) => {
             <div className="mx-4 my-2 flex-grow-1">
                 {weapons.length === 0 ?
                 <p className="text-center fst-italic">nothing selected</p> :
-                <table className="table" style={{fontSize: '.8rem'}}>
+                <table className="table align-middle" style={{fontSize: '.8rem'}}>
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Name</th>
-                            <th scope="col">value</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Value</th>
                             <th scope="col" style={{width: '100%'}}></th>
                         </tr>
                     </thead>
@@ -33,8 +34,9 @@ const BarGraph = ({ weapons, title, toValue, invert, ...props }) => {
                         return <tr key={w.name} > 
                             <td>{i+1}</td>
                             <td style={{whiteSpace: 'nowrap'}}>{w.name}</td>
+                            <td style={{whiteSpace: 'nowrap', fontSize: '.6rem'}}>{w.type}</td>
                             <td>{isNaN(value) ? 'NaN' : value}</td>
-                            <td style={{verticalAlign: 'middle'}}>
+                            <td>
                                 {!isNaN(value) && <div className="progress" style={{margin: '0', height: '.5rem'}}>
                                     <div className="progress-bar" role="progressbar" style={{backgroundColor: '#019b9b', width: (value === Infinity ? maxValue : value) / maxValue*100+'%'}}></div>
                                 </div>}
